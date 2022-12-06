eb362007f708e21f88c6eb8a780ac3c835a0d640
sunuwars77 <sunuwars762@gmail.com>
2022-12-06 22:43:33 +0545

    automation lab

--- automation_basic.py
+++ automation_basic.py
@@ -1,14 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
 import time
 
 
 def open_url():
     s = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=s)
-    driver.get("https://saayog.com")
-    time.sleep(3)
+    driver.maximize_window()
+    driver.get("https://google.com")
+    google_in_nepali = driver.find_element(By.XPATH, '//div[@id="SIvCob"]')
+    actual_text = google_in_nepali.text
+    expected_text = "Google यी भाषामा उपलब्ध छ: English"
+    try:
+        assert actual_text == expected_text
+    except AssertionError:
+        print("footer text comparion test fail")
+    else:
+        print("footer text comparison pass")
+
+    search_box = driver.find_element(By.NAME, 'q')
+    search_box.send_keys(actual_text)
+    search_button = driver.find_element(By.XPATH, '//div[@class="FPdoLc lJ9FBc"]/center/input[@name="btnK"]')
+    search_button.click()
+    time.sleep(1)
+    actual_title = driver.title
+    expected_title = "Nepal - Google खोजी"
+    try:
+        assert actual_title == expected_title
+    except AssertionError:
+        print("title  comparion fail")
+    else:
+        print("title comparison pass")
+
     driver.quit()
 
 
